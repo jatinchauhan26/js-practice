c61e295c75f01714c42158c97f1c19a322964f81
Jatin Chauhan <jatinrajput262005@gmail.com>
2026-03-28 12:11:58 +0100

    Initial commit

---
+++ first.js
@@ -0,0 +1,11 @@
+console.log("hello jatin");  // for print
+console.log("hello world");
+let name = "jatin";          // let for decration variable : can't redeclared but can be updated
+                               
+console.log(name);
+
+const PI = 3.14;   //value can't be redeclared or updated 
+console.log(PI);
+
+
+
---
+++ objects.js
@@ -0,0 +1,27 @@
+const student = {
+  age: 17,
+  fullName : "Rahul Kumar",
+ cgpa : 8.2 ,
+ isPass :true 
+}; 
+student["name"] = "rahul sharma";
+student["age"] = student["age"]+1;
+console.log(student);
+
+
+const Product = {
+   name : "ball pen",
+   rating : 4,
+   isDeal : true,
+   price : 270,
+   offer : 5, 
+};
+console.log(Product);
+
+
+const  profile = {
+ name : "thakurr_jatin_rajput",
+ isFollow : true,
+ noofPosts : 6,
+ bio : "jai shree ram",
+};
