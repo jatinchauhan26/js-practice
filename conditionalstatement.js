6f154b425f864cca0cacbc43704fef2d7eb900b8
Jatin Chauhan <jatinrajput262005@gmail.com>
2026-03-30 01:21:49 +0530

    practice JS operators and conditional statements

---
+++ conditionalstatement.js
@@ -0,0 +1,69 @@
+let age = 14 ;
+if(age >= 18){
+    console.log("you can vote");
+}
+else {
+    console.log("you can't vote");
+}
+
+
+
+
+
+// let mode = "blue ";
+// let color;
+// if(mode === "dark"){
+//     color = "black";
+// }
+// else {
+//    color = "white";
+// }
+// console.log(color);
+
+
+
+////ELSE STATEMENTS 
+let num = 4;
+if(num % 2 == 0){
+    console.log(num ,"id even ");
+}
+else {
+    console.log("num is odd");
+}
+
+
+
+/////ELSE IF STATEMENTS
+
+// let mode = "yellow";
+// let color;
+
+// if(mode === "dark"){
+//     color = "black";
+// }
+// else if (mode == "yellow"){
+//     color = "yellow";
+// }
+// else {
+//     color = "blue"
+// }
+// console.log(color);
+
+
+
+
+let pencolor = "blue";l
+let result = pencolor == "black" ? "yes" : "no";
+console.log(result);
+
+
+
+
+// alert("Enter the number");
+let number = prompt("enter the number");
+if(number%5 == 0){
+    console.log(number ,"is divisible of 5")
+}
+else {
+    console.log(number , "is not divisible by 5")
+}
---
+++ operators.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 5;
+
+
+// a += 4;
+// a **= 4;
+// console.log("a = ",a);
+
+
+let cond1 = a == b;
+let cond2 = a == 2;
+console.log(!(cond1 && cond2));
+
+
+
+
