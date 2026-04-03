23e4f4bac376739fc362a7377b63d9253d3b01b7
Jatin Chauhan <jatinrajput262005@gmail.com>
2026-04-03 23:53:48 +0530

    modified operator.js

--- operators.js
+++ operators.js
@@ -1,16 +1,134 @@
-let a = 5;
-let b = 5;
 
 
-// a += 4;
+//  ARITHMATIC OPERATORS
+
+// let a = 5;
+// let b = 2;
+// console.log("a+b= ",a+b);
+// console.log("a-b= ", a-b);
+// console.log("a*b = ", a*b);
+// console.log("a**b = " , a**b );
+// console.log("a/b = ", a/b);
+// console.log("a%b = ", a%b);
+// console.log("a = ", a , "& b = ",b);
+
+//  UNARY OPERATORS 
+
+// let a = 5 ;
+// let b = 2;
+
+// console.log("a = " ,a , "&b = " , b);
+
+
+// console.log("a-- = ",a--);
+// // console.log("a++ = ",a++);
+// // console.log("++a = ",++a);
+// // console.log("--a =",--a);
+// console.log(a);
+
+// ASSIGNMENT OPERATORS
+
+// let a = 5 ;
+// let b = 4;
 // a **= 4;
-// console.log("a = ",a);
+// console.log("a = ", a);
+
+//  COMPARISON OPERATORS 
+
+// a = 5 ;
+// b = 5 ;
+
+// console.log("a==b",a == b);
+// console.log("a1!= b", a!= b);
+// console.log(a<=b);
+// a = 5 ;
+// b =" 5" ;
+// console.log("a== b", a == b);
 
 
-let cond1 = a == b;
-let cond2 = a == 2;
-console.log(!(cond1 && cond2));
+// a = 5 ;
+// b =" 5" ;
+// console.log("a== b", a === b);
+
+// let color;
+// if(mode == "dark-mode"){
+//     color : "black";
+// }
+
+// CONDITIONAL STATEMENTS 
+
+// let age = 25 ;
+// if (age>18){
+//     console.log("you can vote");
+// }
+
+
+// let mode = "dark";
+// let color;
+
+// if(mode === "dark"){
+//     color = "blue";
+// }
+
+// if(mode === "light"){
+//     color = "white";
+// }
+
+// console.log(color);
+
+// let color ;
+// let mode = "dark";
+// if (mode === "dark"){
+//     color = "black";
+// }
+// else {
+//     color = "white";
+// }
+// console.log(color)
+
+// let age = 18;
+// if (age >= 18 ){
+//     console.log("able to vote")
+// }
+// else {
+//     console.log("not able to vote") 
+// }
+
+// let num = 0 ;
+// if(num%2 == 1){
+//     console.log("num is even")
+// }
+// else if (num==0) {
+//     console.log("num is zero")
+// }
+// else{
+//     console.log("num is odd")
+// }
+
+// syntax -> rules 
+
+// TERNARY OPERATORS : condition? true output : false output
+
+// IST WAY 
+// let age = 19;
+// let result = age > 18 ? "adult" : "notadult";
+// console.log(result);
+
+
+// let age = 19;
+// let result = age > 18 ? console.log("adult") : console.log("notadult");
 
 
 
+// ALERT AND PROMPT
+
+// alert ("hello")
+
+// num = prompt("enter a number");
+// if(num%5===0 ){
+//     console.log(num,"is divisible by 5")
+// }
+// else{
+//     console.log("not divisible")
+// }
 
