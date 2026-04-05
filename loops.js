f350e3db060eec30809e28f7e2eaff1a122cab24
Jatin Chauhan <jatinrajput262005@gmail.com>
2026-04-05 23:56:09 +0530

    practice more topics

---
+++ loops.js
@@ -0,0 +1,41 @@
+//------------LOOPS
+
+// let gameNum = 25;
+// let userNum = prompt("Enter the number");
+//  while(userNum != gameNum){
+//     userNum = prompt("You guessed the wrong number. Guess again");
+
+//  } 
+
+//  console.log("Congratulations, You entered the correct number");
+
+
+
+
+ //--------------------TEMPLATE LITERALS :
+
+
+//  let str = " apnacollege";
+//  console.log(str);
+
+
+// let obj = {
+//     item : "pen",
+//     name : "parker",
+//     color : "blue"
+// };
+
+
+// let result = `item is ${obj.item} , name of pen is ${obj.name}`;
+// console.log(result);
+// console.log("the item name is ",obj.item , "the colour of item is ", obj.color);
+
+
+
+//--------------ESCAPE CHARACTERS ---  for escape character length increse by one 
+
+//    /n - for new line
+//    /t - for tab space
+
+
+
---
+++ string.js
@@ -0,0 +1,26 @@
+//----------STRING METHODS 
+
+
+let str = "Jatin chauhan";
+let str1 = "Jatin";
+let str2 = "Chauhan";
+// let newStr = str.toUpperCase();      //UpperCase
+
+// let newStr =str.toLowerCase();       //LowerCase
+
+// console.log(str.trim());             //Trim the extra space  
+
+// console.log(str.slice(1,3));          //IN slice ending value not include 
+// console.log(str.slice(3));             
+
+// let result = str1.concat(str2);       //concatenation
+// console.log(result);
+// console.log(str1+str2);
+
+// console.log(str.replace("a","p"))         //Replace characters
+// console.log(str.replaceAll("a","p"))
+
+console.log(str.charAt(3));
+
+// console.log(newstr);
+
