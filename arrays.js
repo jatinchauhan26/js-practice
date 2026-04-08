91832fd8fff3d275ad5d5524ec57373391c94def
Jatin Chauhan <jatinrajput262005@gmail.com>
2026-04-09 00:57:52 +0530

    practiced arrays today

---
+++ arrays.js
@@ -0,0 +1,50 @@
+// let marks = [1,2,34,5,6,4];
+//  marks[1] = 4;
+// console.log(marks)
+
+// let heroes = ["jatinbhai","jatindon","jatinbadmosh","dangerjatin"]
+// // for(let i=0 ; i< heroes.length ; i++){
+// //     for(let j = 0 ; j< heroes.length ;j++)
+// // console.log(heroes[j]);
+// // }
+
+// for (let i = 0; i < heroes.length; i++) {
+//     let word = heroes[i];
+
+//     for (let j = 0; j < word.length; j++) {
+//         console.log(word[j]);   // prints each character
+//     }
+// }
+
+
+
+// let marks = [85,97,44,37,76,60];
+// let sum = 0;
+// for(let i = 0 ; i<marks.length ;i++){
+//     sum += marks[i] ;
+
+// }
+// let  average_sum  = sum / marks.length;
+// console.log(average_sum);
+
+ 
+
+// Question ::: 
+//For a given array with prices of 5 items.
+// All items have an offer of 10% OFF on them . Change the array to store final price agter applying offer.
+
+
+let items = [250,645,300,900,50];
+// let i = 0;
+// for(let val of items){
+//     let discount = (val*10)/100;
+//     items[i] = val - discount;
+//     i++
+// }
+
+
+for(let i = 0 ; i< items.length ; i++){
+    let discount = (items[i]*10)/100;
+    items[i] -= discount ; 
+console.log("now the item after discount are :" , items[i]);
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+let heroes = ["jatinbhai","jatindon","jatinbadmosh","dangerjatin"]
+for(let i=0 ; i< heroes.length ; i++){
+console.log(heroes[i]);
+}
